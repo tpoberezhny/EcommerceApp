ce82505d0c3ff8ef98a36415096298fce65fe5eb
Tim <poberezhny003@gmail.com>
2024-09-27 18:30:15 +0200

    Auth route

---
+++ routes/auth.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const bcrypt = require("bcrypt");
+const db = require("../db/index");
+
+const router = express.Router();
+
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      //Fetching user from db
+      const result = await db.query("SELECT * FROM users WHERE username = $1", [
+        username,
+      ]);
+
+      const user = result.rows[0];
+      const isPasswordMatch = await bcrypt.compare(password, user.password);
+
+      if (!user || !isPasswordMatch) {
+        return done(null, false, { message: "Incorrect credentials." });
+      }
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  })
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const result = await db.query("SELECT * FROM users WHERE id = $1", [id]);
+    const user = result.rows[0];
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/dashboard",
+    failureRedirect: "/login",
+    failureFlash: false,
+  })
+);
+
+router.get("/dashboard", (req, res) => {
+  if (req.isAuthenticated()) {
+    res.send(`Welcome ${req.user.username}! This is your dashboard.`);
+  } else {
+    res.redirect("/login");
+  }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,26 @@
-const express = require('express');
+const express = require("express");
+const session = require('express-session');
+const passport = require('passport');
+require('dotenv').config();
 
 const app = express();
 const port = 3000;
-
 app.use(express.json());
 
-const registerRoute = require('./routes/register');
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+const registerRoute = require("./routes/register");
 app.use(registerRoute);
+
+const authRoute = require('./routes/auth');
+app.use(authRoute);
 
 app.listen(port, () => {
   console.log(`Server is running on PORT:${port}`);
