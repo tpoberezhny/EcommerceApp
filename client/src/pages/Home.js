7ebf2da671b4f44ca5873e197795c29a46087660
Tim <poberezhny003@gmail.com>
2024-10-11 19:32:51 +0200

    Basic Home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "../styles/Home.scss";
 
 const Home = () => {
-  return <h1>Home Page</h1>;
+  return (
+    <div className="first-section">
+      <h1 className="title">Shop Smart, Save Big with Our App</h1>
+      <p>
+        Discover unbeatable deals on a wide range of products tailored just for
+        you. Experience
+      </p>
+      <p className="description-text">
+        seamless shopping and fast delivery at your fingertips
+      </p>
+      <Link to="/products" className="shopBtn">
+        Shop
+      </Link>
+    </div>
+  );
 };
 
 export default Home;
