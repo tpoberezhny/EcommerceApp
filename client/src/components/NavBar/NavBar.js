0296c8cd22334c030fa6638aa8e1edbbd79184dd
Tim <poberezhny003@gmail.com>
2024-10-13 15:26:58 +0200

    Removing not needed dependencies

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./NavBar.scss";
 
 import { Link, useNavigate } from "react-router-dom";
@@ -13,7 +13,7 @@ const Navbar = ({
   username,
   setIsAuthenticated,
   setUsername,
-  cart
+  cart,
 }) => {
   const navigate = useNavigate();
 
