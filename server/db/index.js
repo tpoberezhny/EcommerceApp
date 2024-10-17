fa5b2eaafb124aaa4c08fbb0a3e1c21a71eef4db
Tim <poberezhny003@gmail.com>
2024-10-17 13:37:10 +0200

    Enabling ssl

--- server/db/index.js
+++ server/db/index.js
@@ -7,6 +7,9 @@ const pool = new Pool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = {
