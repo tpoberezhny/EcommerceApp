7b01dc2ca517f791406b4ac14f4167b4043b86f2
Tim <poberezhny003@gmail.com>
2024-11-03 11:06:18 +0100

    Reducing time of ping

--- server/keepAlive.js
+++ server/keepAlive.js
@@ -2,9 +2,16 @@ const axios = require("axios");
 
 // Ping backend every 1 minute
 setInterval(() => {
-    axios.get(process.env.baseURL)
-        .then(() => console.log("Ping successful to keep backend alive"))
-        .catch((error) => console.log("Ping failed:", error.message));
-}, 60 * 1000);
+  axios
+    .get(process.env.baseURL)
+    .then((response) => {
+      console.log(
+        `Reloaded at ${new Date().toISOString()}: Status Code ${
+          response.status
+        }`
+      );
+    })
+    .catch((error) => console.log("Ping failed:", error.message));
+}, 30000);
 
 module.exports = {};
